6bd42102f113c3c3d1729bb16961dca83a042b10
Vivek Trivedi <69842867+vivektrivedi-123@users.noreply.github.com>
2022-05-23 16:07:49 +0530

    Update sample.tcl

--- sample.tcl
+++ sample.tcl
@@ -1,5 +1,8 @@
 #!/usr/bin/expect
 #Start the guest VM
+
+set workspace [lindex $argv 2]
+
 eval spawn  ssh -i $workspace/.ssh/id_rsa kubernetes@192.168.0.131 kubectl get pods -n erp
 
 set timeout 20
